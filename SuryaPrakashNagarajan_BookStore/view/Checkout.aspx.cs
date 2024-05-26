98ae5c3f6e7c5a83275488ad5a09d26bba281774
peihome <ssuurryyaa101@gmail.com>
2024-05-26 17:33:51 -0400

    Added Checkout.aspx

---
+++ SuryaPrakashNagarajan_BookStore/view/Checkout.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SuryaPrakashNagarajan_BookStore.view
+{
+    public partial class Checkout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery.validate.unobtrusive.min.js",
+                DebugPath = "~/Scripts/jquery.validate.unobtrusive.min.js"
+            });
+
+            if (!IsPostBack)
+            {
+                PopulateStateList();
+            }
+        }
+
+        protected void PopulateStateList()
+        {
+            State.Items.Add(new ListItem("--select--", ""));
+            State.Items.Add(new ListItem("Alberta", "Alberta"));
+            State.Items.Add(new ListItem("British Columbia", "British_Columbia"));
+            State.Items.Add(new ListItem("Manitoba", "Manitoba"));
+            State.Items.Add(new ListItem("New Brunswick", "New_Brunswick"));
+            State.Items.Add(new ListItem("Newfoundland and Labrador", "Newfoundland_and_Labrador"));
+            State.Items.Add(new ListItem("Nova Scotia", "Nova_Scotia"));
+            State.Items.Add(new ListItem("Ontario", "Ontario"));
+            State.Items.Add(new ListItem("Prince Edward Island", "Prince_Edward_Island"));
+            State.Items.Add(new ListItem("Quebec", "Quebec"));
+            State.Items.Add(new ListItem("Saskatchewan", "Saskatchewan"));
+            State.Items.Add(new ListItem("Northwest Territories", "Northwest_Territories"));
+            State.Items.Add(new ListItem("Nunavut", "Nunavut"));
+            State.Items.Add(new ListItem("Yukon", "Yukon"));
+        }
+
+        protected void CheckOut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
